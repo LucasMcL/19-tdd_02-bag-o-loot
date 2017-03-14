8976e92afef155da7a339fd39a4abd004112709f
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-14 12:00:18 -0500

    parseArgs passing all tests

--- src/parse-args.js
+++ src/parse-args.js
@@ -9,20 +9,51 @@
 //  - ls
 //  - ls suzy
 //  - delivered suzy
-//
-//  Check first word
-//  	- if not "add", "remove", "ls", or "delivered", that's an error
-//
 
 // Can a toy be added to a child before it exists in the toys table?
 // Can we create the toy, then add it to the child in one operation?
 
 // Method should return error argument (may be null) and arguments from user optionally proccessed
 
-const parseArgs = function(args) {
+/**
+ * processes user input and returns on object with error (may be null) and args
+ * @param  {array} args - array of user input arguments
+ * @return {object}     - object containing err and args
+ */
+const checkArgs = function(args) {
+	let [command, arg2, arg3] = args
+
 	let err = null
+	const validCommands = ['add', 'remove', 'ls', 'delivered']
+	if(!command) {
+		err = new Error('Please enter a command')
+		return {err, args}
+	}
+	if(!validCommands.includes(command)) {
+		err = new Error('Not a valid command.  Please begin your request with "add", "remove", "ls", or "delivered"')
+		return {err, args}
+	}
+	if(command === 'remove') {
+		if(!arg3) {
+			err = new Error('Please specify child and toy to remove.  Example: "remove michael baseball".')
+		}
+	}
+
 
 	return {err, args}
 }
 
-module.exports = {parseArgs}
+module.exports = {checkArgs}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,31 @@
 const {assert} = require('chai')
 
 // Testing for parse arguments method
-const {parseArgs} = require('../src/parse-args.js')
-describe('parseArgs method', function() {
+const {checkArgs} = require('../src/parse-args.js')
+describe('checkArgs method', function() {
 	it('should return an object', function() {
-		assert.isObject(parseArgs())
+		assert.isObject(checkArgs(['dumbUserInput']))
 	})
 	it('should be a function', function() {
-		assert.isFunction(parseArgs)
+		assert.isFunction(checkArgs)
 	})
 
 	describe('return an error when', function() {
 		it('the first argument is not within the list of correct words', function() {
-			const {err} = parseArgs(['puppies', 'suzy', 'add'])
+			const {err} = checkArgs(['dumbUserInput'])
 			assert.isNotNull(err)
+			assert.isDefined(err)
 		})
-		it('should ...', function() {
-
+		it('there were no arguments passed', function() {
+			const {err} = checkArgs([])
+			assert.isNotNull(err)
+			assert.isDefined(err)
+		})
+		it('the user tries to remove without specifying toy and child', function() {
+			const {err: err1} = checkArgs(['remove', 'toy'])
+			const {err: err2} = checkArgs(['remove', 'michael'])
+			assert.isNotNull(err1); assert.isNotNull(err2)
+			assert.isDefined(err1); assert.isDefined(err2)
 		})
 	})
 })
