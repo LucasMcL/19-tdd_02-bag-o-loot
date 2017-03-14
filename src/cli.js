3dc623be1da90eb9d53330b6370ed43d6c45ac18
LucasMcL <LMcLaughlin09@gmail.com>
2017-03-14 11:03:45 -0500

    planned, added comments with responsibilities for each file

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+'use strict'
+
+// Process the arguments passed in from the user
+// Load other modules and call appropriate methods
+
---
+++ src/parse-args.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// This module exports a method used for parsing arguments
+// This method will be used in the cli file
+
+// Possible inputs from user:
+// 	- add baseball michael
+//  - remove michael baseball
+//  - ls
+//  - ls suzy
+//  - delivered suzy
+//
+//  Check first word
+//  	- if not "add", "remove", "ls", or "delivered", that's an error
+//
